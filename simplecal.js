1729c3e44fa219f225670a831c44b5f3df1fad1b
Nandhini <nandhinikethavath95@gmail.com>
2026-05-04 14:42:07 +0530

    simplecal

---
+++ simplecal.js
@@ -0,0 +1,20 @@
+let num1=45
+let num2=67
+let operator="+";
+
+switch(operator) {
+    case "+":
+        console.log(num1+num2)
+        break;
+    
+    case "-":
+        console.log(num1-num2)
+        break;
+
+    case "*":
+        console.log(num1*num2)
+        break;
+
+    case "/":
+        console.log(num1/num2)
+}
